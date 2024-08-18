f71832dfe35cde2a35d979af5bf344c247c5c9cc
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-08-19 00:04:57 +0530

    clean up

--- frontend/src/lib/config.ts
+++
@@ -1,8 +0,0 @@
-import { bscTestnet } from "viem/chains";
-import { http, createConfig } from "wagmi";
-export const config = createConfig({
-  chains: [bscTestnet],
-  transports: {
-    [bscTestnet.id]: http(),
-  },
-});
